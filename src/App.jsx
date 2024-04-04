0de2543e0204307fb95f410e6bd9f94ce643dfed
BryanGordon <bryanbge92@gmail.com>
2024-04-04 14:33:48 -0500

    Improve clean code
    Improved clean code on all files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,153 +1,152 @@
-import React,{useState,useEffect} from "react";
-import {store} from './firebaseConfig'
+import React, { useState, useEffect } from 'react'
+import { store } from './firebaseConfig'
 
-function App() {
+function App () {
+  const [idUsuario, setIdUsuario] = useState('')
+  const [edicion, setEdicion] = useState (null)
+  const [nombre, setNombre] = useState('')
+  const [telefono, setTelefono] = useState('')
+  const [usuarios, setUsuarios] = useState([])
+  const [msgError, setMsgError] = useState('')
 
-  const[idUsuario,setIdUsuario]=useState('')
-  const[edicion,setEdicion]=useState(null)
-  const[nombre,setNombre]=useState('')
-  const[telefono,setTelefono]=useState('')
-  const[usuarios,setUsuarios]=useState([])
-  const[msgError,setMsgError]=useState('')
-
-  useEffect(()=>{
-    
-    const getUsuarios=async()=>{
-      const {docs}=await store.collection('contactos').get()
-      const array=docs.map(item=>({id:item.id,...item.data()}))
+  useEffect(() => {
+    const getUsuarios = async() => {
+      const {docs} = await store.collection('contactos').get()
+      const array = docs.map(item => ({id:item.id,...item.data()}))
       setUsuarios(array)
     }
     getUsuarios()
-  },[])
+  }, [])
 
-  const setUsuario= async (e)=>{
+  const setUsuario = async(e) => {
     e.preventDefault()
 
-    if(!nombre.trim()){
+    if(!nombre.trim()) {
       setMsgError('El campo nombre esta vacio')
     }
-    if(!telefono.trim()){
+    if(!telefono.trim()) {
       setMsgError('El campo telefono esta vacio')
     }
-    const usuario={
-      nombre:nombre,
-      telefono:telefono
+
+    const usuario = {
+      nombre: nombre,
+      telefono: telefono
     }
-    try{
-      const data=await store.collection('contactos').add(usuario)
-      const {docs}=await store.collection('contactos').get()
-      const array=docs.map(item=>({id:item.id,...item.data()}))
+
+    try {
+      const data = await store.collection('contactos').add(usuario)
+      const {docs} = await store.collection('contactos').get()
+      const array = docs.map(item => ({id:item.id,...item.data()}))
       setUsuarios(array)
       alert("Usuario añadido")
-      
-    }catch(e){
+    } catch(e) {
       console.log(e)
     }
+
     setNombre('')
     setTelefono('')
   
   }
 
-  const BorrarUsuario=async (id)=>{
-    try{
+  const BorrarUsuario = async(id)=>{
+    try {
       await store.collection('contactos').doc(id).delete()
-      const {docs}=await store.collection('contactos').get()
-      const array=docs.map(item=>({id:item.id,...item.data()}))
+      const {docs} = await store.collection('contactos').get()
+      const array = docs.map(item => ({id:item.id,...item.data()}))
       setUsuarios(array)
       alert('Usuario eliminado')
-    }
-    catch(e){
+    } catch(e) {
       console.log(e)
     }
+
   }
 
-  const SeleccionarUsuario= async (id)=>{
- 
-    try{
-      const data=await store.collection('contactos').doc(id).get()
-      const usuario=data.data()
+  const SeleccionarUsuario = async(id) => {
+    try {
+      const data = await store.collection('contactos').doc(id).get()
+      const usuario = data.data()
       setNombre(usuario.nombre)
       setTelefono(usuario.telefono) 
       setIdUsuario(usuario.id)
       setEdicion(true)
-    }
-    catch(e){
+    } catch(e) {
       console.log(e)
     }
 
   }
 
-  const ActualizarUsuario= async(e)=>{
-
+  const ActualizarUsuario = async(e) => {
     e.preventDefault()
 
-    if(!nombre.trim()){
+    if(!nombre.trim()) {
       setMsgError('El campo nombre esta vacio')
     }
-    if(!telefono.trim()){
+    if(!telefono.trim()) {
       setMsgError('El campo telefono esta vacio')
     }
-    const usuarioUpdate={
+
+    const usuarioUpdate = {
       nombre:nombre,
       telefono:telefono
     }
 
-    try{
+    try {
       await store.collection('contactos').doc(idUsuario).set(usuarioUpdate)
-      const {docs}=await store.collection('contactos').get()
-      const array=docs.map(item=>({id:item.id,...item.data()}))
+      const {docs} = await store.collection('contactos').get()
+      const array = docs.map(item=>({id:item.id,...item.data()}))
       setUsuarios(array)
       alert("Usuario actualizado")
       
-    }catch(e){
+    } catch(e) {
       console.log(e)
     }
+    
     setNombre('')
     setTelefono('')
     setIdUsuario('')
     setEdicion(false)
-  
+
   }
   
-
   return (
     <div className='container'>
       <div className='row'>
         <div className="col">
           <h2>Formulario de usuarios</h2>
-          <form className='form-group' onSubmit={edicion? ActualizarUsuario: setUsuario}>
+          <form className='form-group' onSubmit={edicion ? ActualizarUsuario : setUsuario}>
             <input type="text"
               value={nombre} 
               placeholder="Introduce el nombre"
               className='form-control'
-              onChange={(e)=>{setNombre(e.target.value)}}
+              onChange={(e) => {setNombre(e.target.value)}}
             />
             <input type="text"
               value={telefono}
               placeholder="Introduce el telefono"
               className='form-control mt-3'
-              onChange={(e)=>{setTelefono(e.target.value)}}
+              onChange={(e) => {setTelefono(e.target.value)}}
             />
             {
-              edicion?
+              edicion
+              ?
               (
                 <input type="submit" 
                   value='Editar'
                   className='btn btn-dark btn-block mt-4 mb-4' 
-              />
+                />
               )
               :
               (
                 <input type="submit" 
                   value='Registrar'
                   className='btn btn-dark btn-block mt-4 mb-4' 
-            />
+                />
               )
             }
-            
           </form>
           {
-            msgError?
+            msgError
+            ?
             (
               <div>
                 {msgError}
@@ -163,14 +162,15 @@ function App() {
           <h2>Lista de contactos</h2>
           <ul className='list-group'>
             {
-            usuarios.length !==0?
+            usuarios.length !==0
+            ?
             (
-              usuarios.map(item=>(
+              usuarios.map(item => (
                 <li key={item.id}
                   className='list-group-item'
                 >
-                  <button onClick={(id)=>{BorrarUsuario(item.id)}} className='btn btn-danger float-right'>Borrar</button>
-                  <button onClick={(id)=>{SeleccionarUsuario(item.id)}} className='btn btn-info float-right mr-2'>Actualizar</button>
+                  <button onClick={(id) => {BorrarUsuario(item.id)}} className='btn btn-danger float-right'>Borrar</button>
+                  <button onClick={(id) => {SeleccionarUsuario(item.id)}} className='btn btn-info float-right mr-2'>Actualizar</button>
                   <label>Nombre: {item.nombre}</label>
                   <br/>
                   <label>Telefono: {item.telefono}</label>
@@ -188,8 +188,7 @@ function App() {
         </div>
       </div>
     </div>
-  );
+  )
 }
-
 
 export default App;
